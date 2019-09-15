aea512be079021eab7d3f53772865df52428a1e3
imranrazakhan <imranrazakhan@users.noreply.github.com>
2019-09-15 14:24:47 +0200

    Update ConfigMapWatchClient.java

--- src/main/java/com/imran/kubernetes/configmap/ConfigMapWatchClient.java
+++ src/main/java/com/imran/kubernetes/configmap/ConfigMapWatchClient.java
@@ -7,20 +7,20 @@ import org.apache.camel.main.Main;
 public final class ConfigMapWatchClient {
 	
 	public static void main(String[] args) throws Exception {
-        Main main = new Main();
-        main.bind("trustCert", new HttpClientConfigurerTrustAllCACerts());
-        main.addRouteBuilder(new ConfigMapWatchClientRouter());
-        main.run(args);
-    }
+        	Main main = new Main();
+        	main.bind("trustCert", new HttpClientConfigurerTrustAllCACerts());
+        	main.addRouteBuilder(new ConfigMapWatchClientRouter());
+        	main.run(args);
+    	}
 	
 	private static class ConfigMapWatchClientRouter extends RouteBuilder {
         @Override
         public void configure() throws Exception {
              from("timer://foo?fixedRate=true&period=60000")
-             .setHeader("Authorization", simple("Bearer sXoZfVfMW2f4nWjiFsOGjf56B70nEjr18LX1c4bHHVo"))
+             .setHeader("Authorization", simple("Bearer xxx"))
              .setHeader("Accept", simple("application/json"))
              .setHeader("CamelHttpMethod", constant("GET"))
-             .to("https4://m.devokd.younicos.local:8443/api/v1/watch/namespaces/yq-qa/configmaps/my-config-map?httpClientConfigurer=#trustCert")
+             .to("https4://localhost:8443/api/v1/watch/namespaces/my-qa/configmaps/my-config-map?httpClientConfigurer=#trustCert")
              .to("log:my?showAll=true&multiline=true")
              .to("mock:result");
         }
